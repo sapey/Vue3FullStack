8e625d0e3b76884e08511bc77b725edc4c62211c
sapey <60222927+sapey@users.noreply.github.com>
2022-06-20 16:29:56 +0200

    création navbar + page todoList

--- Vue3FullStack/src/App.vue
+++ Vue3FullStack/src/App.vue
@@ -3,7 +3,17 @@ import { RouterView } from "vue-router";
 
 </script>
 
+
 <template>
+
+<div id="navbar">
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/signIn">signIn</a>
+  <a href="/signUp">signUp</a>
+  <a href="/todolists">Todo list</a>
+</div>
+  
   <RouterView />
 </template>
 
